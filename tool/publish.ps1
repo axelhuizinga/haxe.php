b5c7fc7a71dc068dc7fb9d446dc3ffd829aac17b
Cédric Belin <cedric@belin.io>
2020-05-11 15:59:08 +0200

    Updated the "publish" task

--- tool/publish.ps1
+++ tool/publish.ps1
@@ -3,6 +3,5 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 $version = (Get-Content composer.json | ConvertFrom-Json -AsHashTable).version
-tool/version.ps1
 git tag "v$version"
 git push origin "v$version"
