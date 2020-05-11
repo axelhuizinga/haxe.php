b1c4dd13c50d6e7dc2ff5b94bee41ec72494af41
Cédric Belin <cedric@belin.io>
2020-05-11 15:51:09 +0200

    Fixed the build system

--- tool/version.ps1
+++ tool/version.ps1
@@ -2,5 +2,5 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
-$version = (Get-Content composer.json | ConvertFrom-Json).version
+$version = (Get-Content composer.json | ConvertFrom-Json -AsHashTable).version
 (Get-Content etc/phpdoc.xml) -replace 'version number="\d+(\.\d+){2}"', "version number=""$version""" | Out-File etc/phpdoc.xml
