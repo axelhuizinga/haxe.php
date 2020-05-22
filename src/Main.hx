ca996a630ffe426581082224bb7588f322d6f9c6
Cédric Belin <cedric@belin.io>
2020-05-22 16:33:34 +0200

    Updated the classes to Haxe 4.1.1

--- src/Main.hx
+++ src/Main.hx
@@ -2,24 +2,38 @@ import haxe.*;
 import sys.*;
 import sys.io.*;
 
+using DateTools;
 using Lambda;
 using StringTools;
 
 /** The main class. **/
 @:keep class Main {
-	public var array: Array<Any> = null;
-	public var boolean: Bool = false;
-	public var date: Date = null;
-	public var ereg: EReg = ~/.*/;
-	public var integer: Int = 0;
-	public var number: Float = 0.0;
-	public var string: String = "";
+
+	// Root package.
+	var array: Array<Any> = null;
+	var boolean: Bool = false;
+	var date: Date = null;
+	var ereg: EReg = ~/.*/;
+	var integer: Int = 0;
+	var number: Float = 0.0;
+	var string: String = "";
+	var stringBuffer: StringBuf = null;
+	var unsignedInteger: UInt = 0;
+	var unicodeString: UnicodeString = "";
+	var xml: Xml = null;
+
+	// sys.io package.
+	var process: Process = null;
 
 	/** Application entry point. **/
 	static function main(): Void {
+		// Root package.
 		Math.abs(-1);
 		Reflect.hasField({}, "foo");
 		Std.isOfType("", String);
 		Sys.exit(0);
+
+		// sys.io package.
+		File.getContent("build.hxml");
 	}
 }
