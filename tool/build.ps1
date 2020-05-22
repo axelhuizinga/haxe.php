2989d83f0e94974d57ec4837250eac46c7b65739
Cédric Belin <cedric@belin.io>
2020-05-22 16:21:01 +0200

    Updated the build system

--- tool/build.ps1
+++ tool/build.ps1
@@ -3,5 +3,6 @@ Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
 haxe build.hxml
-Remove-Item src/index.php
-Remove-Item src/Main.php
+foreach ($item in "src/index.php", "src/Main.php") {
+	if (Test-Path $item) { Remove-Item $item }
+}
