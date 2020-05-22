81f9210f354d7483258219fe7c57f3885022289a
Cédric Belin <cedric@belin.io>
2020-05-22 16:53:21 +0200

    Added new classes

--- Main.hx
+++ Main.hx
@@ -29,6 +29,10 @@ using haxe.EnumTools;
 	// "haxe" package.
 	var exception: Exception = null;
 	var serializer: Serializer = null;
+	var template: Template = null;
+	var timer: Timer = null;
+	var unserializer: Unserializer = null;
+	var valueException: ValueException = null;
 
 	// "sys.io" package.
 	var process: Process = null;
